4219eceba258b74aff81da2367e5ee78f301e7dc
ando <anderson@easil.com>
2020-01-28 02:27:09 -0300

    deploy

---
+++ precache-manifest.bf96c7bfe3bd6f8ccf0acb7fc1e12b6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52131ae2742cbddcd9bd751b8bf68add",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd36c3158ac14bf18cd",
+    "url": "/static/css/main.cd6100c9.chunk.css"
+  },
+  {
+    "revision": "2c9b85b96b5490ce4820",
+    "url": "/static/js/2.7afcee2a.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/static/js/2.7afcee2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ddd36c3158ac14bf18cd",
+    "url": "/static/js/main.8d6c3268.chunk.js"
+  },
+  {
+    "revision": "38cc8ad1f1355a3fc28b",
+    "url": "/static/js/runtime-main.fd0b33e8.js"
+  }
+]);
